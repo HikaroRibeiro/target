9723cae6db276d9cfed9d102cd568f6b24ce0339
HikaroRibeiro <hhakawama@gmail.com>
2026-06-23 13:25:35 -0300

    ✨ Transaction Component
    Finished transaction component.

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,12 +1,57 @@
-import { View, Text, Button } from "react-native";
+import { View } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+import { List } from "@/components/List";
+import { Transaction } from "@/components/Transaction";
+import { TransactionType } from "@/utils/TransactionTypes";
+import { Button } from "@/components/Button";
 
+const details = {
+  current: "R$ 580,00",
+  target: "R$ 1.790,00",
+  percentage: 25
+}
+
+const transactions = [{
+  id: "1",
+  value: "R$ 1.580,00",
+  date: "01/01/2023",
+  description: "Investimentos em Renda Fixa",
+  type: TransactionType.INPUT
+}, 
+{
+  id: "2",
+  value: "R$ 580,00",
+  date: "01/01/2023",
+  description: "Compra de Apple Watch",
+  type: TransactionType.OUTPUT
+}]
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>In Progress {params.id}</Text>
-      <Button title="Voltar" onPress={() => router.back()} />
+    <View style={{ flex: 1, marginTop: 24, padding: 18 }}>
+      <PageHeader 
+        title="Apple Watch"
+        rightButton={{
+          icon: "edit",
+          onPress: () => {}
+        }} />
+
+        <Progress data={details} />
+
+        <List 
+          title="Transações"
+          data={transactions}
+          renderItem={({ item }) => <Transaction data={item} onRemove={() => {}} />}
+          keyExtractor={(item) => item.id}
+          emptyMessage="Nenhuma transação cadastrada. Clique em nova transação para cadastrar!"
+        />
+
+        <Button 
+          title={"Nova Transação"} 
+          isProcessing={false} 
+          onPress={() => router.navigate(`/transaction/${params.id}`)} />
     </View>
   );
 }
---
+++ src/utils/TransactionTypes.ts
@@ -0,0 +1,5 @@
+export enum TransactionType {
+    INPUT = 'input',
+    OUTPUT = 'output',
+    TRANSFER = 'transfer'
+}
